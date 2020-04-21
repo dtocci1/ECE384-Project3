cbb7629fc8d5928e12f97fd17c7bc782928dbf86
dtocci1 <43154755+dtocci1@users.noreply.github.com>
2020-04-21 09:22:35 -0700

    Added main file for project code

---
+++ Project6_1.m
@@ -0,0 +1,18 @@
+%% ECE 384 Project 6.1
+%% Tyler Botelho, Lewis Brum, Matt LaVoie, Dylan Tocci
+clear;clc;
+
+% 1. Generate six sequences of uniform random variables (length  10,000) in the
+% range [−0.5, 0.5]. Each element in the ith sequence can be considered to be one
+% realization of the random variable Xi
+
+
+
+% 2. Generate 20 sequences of IID exponential random variables Xi using parameter
+% lambda = 0.5i for the ith sequence.
+
+
+
+% 3. Successively convolve 50 Bernoulli PMFs (not the random variables). Choose
+% p = 0.8 for all of the PMFs. Plot and compare the shape (only the shape)
+% of the result to that of the Gaussian PDF
